0ea6f68a58a15090d71d0f551c168e40e942f71d
Mazdak Teymourian <119652716+MazMazTey@users.noreply.github.com>
2024-06-25 20:48:58 +0330

    Fixed Parking Bug

--- Code/Parking.v
+++ Code/Parking.v
@@ -13,7 +13,7 @@ module Parking(
     output reg is_vacated_space,
     output wire [11:0] clock_time
 );
-    integer MAX_CAP_PARKING;
+    integer MAX_CAP_PARKING = 700;
     integer MAX_CAP_UNI;
     integer MAX_CAP_GENERAL;
 
@@ -27,36 +27,30 @@ module Parking(
         uni_is_vacated_space <= 1'b1;
         is_vacated_space <= 1'b1;
         if (clock_time < 8) begin
-            MAX_CAP_PARKING = 700;
             MAX_CAP_UNI = 0;
             MAX_CAP_GENERAL = 0;
             uni_is_vacated_space <= 1'b0;
             is_vacated_space <= 1'b0;
         end
-        else if (clock_time <= 13) begin
-            MAX_CAP_PARKING = 700;
+        else if (clock_time < 13) begin
             MAX_CAP_UNI = 500;
             MAX_CAP_GENERAL = 200;
             uni_is_vacated_space <= 1'b1;
             is_vacated_space <= 1'b1;
         end
-        else if (clock_time <= 14) begin
-            MAX_CAP_PARKING = 700;
+        else if (clock_time < 14) begin
             MAX_CAP_UNI = 450;
             MAX_CAP_GENERAL = 250;
         end
-        else if (clock_time <= 15) begin
-            MAX_CAP_PARKING = 700;
+        else if (clock_time < 15) begin
             MAX_CAP_UNI = 400;
             MAX_CAP_GENERAL = 300;
         end
-        else if (clock_time <= 16) begin
-            MAX_CAP_PARKING = 700;
+        else if (clock_time < 16) begin
             MAX_CAP_UNI = 350;
             MAX_CAP_GENERAL = 350;
         end
         else begin
-            MAX_CAP_PARKING = 700;
             MAX_CAP_UNI = 200;
             MAX_CAP_GENERAL = 500;
         end
@@ -73,21 +67,23 @@ module Parking(
             vacated_space = MAX_CAP_GENERAL - parked_care;
             uni_vacated_space = MAX_CAP_UNI - uni_car_parked;
             if (car_entered) begin                                  //check if car entered
-                if (is_uni_car_entered) begin                       //check if it is uni car
-                    if (uni_is_vacated_space) begin                 //check if there is any empty space
-                        uni_car_parked = uni_car_parked + 1;
-                        uni_vacated_space = MAX_CAP_UNI - uni_car_parked;
-                        if (uni_car_parked >= MAX_CAP_UNI) begin    //check if the uni cap is full
-                            uni_is_vacated_space = 1'b0;
+                if (parked_care + uni_car_parked < MAX_CAP_PARKING) begin     
+                    if (is_uni_car_entered) begin                       //check if it is uni car
+                        if (uni_is_vacated_space && uni_vacated_space >= 100) begin                 //check if there is any empty space
+                            uni_car_parked = uni_car_parked + 100;
+                            uni_vacated_space = MAX_CAP_UNI - uni_car_parked;
+                            if (uni_car_parked >= MAX_CAP_UNI) begin    //check if the uni cap is full
+                                uni_is_vacated_space = 1'b0;
+                            end
                         end
                     end
-                end
-                else begin
-                    if (is_vacated_space) begin                     //check for general cars
-                        parked_care = parked_care + 1;
-                        vacated_space = MAX_CAP_GENERAL - parked_care;
-                        if (parked_care >= MAX_CAP_GENERAL) begin
-                            is_vacated_space = 1'b0;
+                    else begin
+                        if (is_vacated_space && vacated_space >= 100) begin                     //check for general cars
+                            parked_care = parked_care + 100;
+                            vacated_space = MAX_CAP_GENERAL - parked_care;
+                            if (parked_care >= MAX_CAP_GENERAL) begin
+                                is_vacated_space = 1'b0;
+                            end
                         end
                     end
                 end
@@ -95,7 +91,7 @@ module Parking(
             else if (car_exited) begin                             //check if car exited
                 if (is_uni_car_exited) begin                       //check if it is uni car
                     if (uni_car_parked > 0) begin    
-                        uni_car_parked = uni_car_parked - 1;
+                        uni_car_parked = uni_car_parked - 100;
                         uni_vacated_space <= MAX_CAP_UNI - uni_car_parked;
                         if (!uni_is_vacated_space) begin                //check if the uni cap is full
                             uni_is_vacated_space = 1'b1;
@@ -104,7 +100,7 @@ module Parking(
                 end
                 else begin                                          //check for general cars
                     if (parked_care > 0) begin
-                        parked_care = parked_care - 1;
+                        parked_care = parked_care - 100;
                         vacated_space <= MAX_CAP_GENERAL - parked_care;
                         if (!is_vacated_space) begin
                             is_vacated_space = 1'b1;
